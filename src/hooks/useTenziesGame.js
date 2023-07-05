4a60e8a6d0a09a9ffd49c4e0f73ee2cb139c9f15
Ivan Vrlić <123493736+ivrlic@users.noreply.github.com>
2023-07-05 22:39:33 +0200

    Update useTenziesGame.js

--- src/hooks/useTenziesGame.js
+++ src/hooks/useTenziesGame.js
@@ -11,7 +11,7 @@ export default function useTenziesGame() {
 
     const [time, setTime] = useState({min:0, sec:0})
     const [timeStatus, setTimeStatus] = useState(false)
-    const [moves, setMoves] = useState(-2)
+    const [moves, setMoves] = useState(-1)
     const [dice, setDice] = useState(getFirstNewDice())
     const [tenzies, setTenzies] = useState(false)
     const [score, setScore] = useState(0)
